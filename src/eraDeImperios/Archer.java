d48f756b48e277c393d16beba7a502e616fa660f
Diego <diegoo.villalba@gmail.com>
2021-11-20 22:52:31 -0300

    Version 2.0.1. Se agregan flechas a los arqueros y cantidad de ataques a los soldados

--- src/eraDeImperios/Archer.java
+++ src/eraDeImperios/Archer.java
@@ -3,6 +3,7 @@ package eraDeImperios;
 public class Archer extends Unity {
 
 	private int damage = 7;
+	private int arrows = 10;
 
 	public Archer(Position position) {
 		super(position);
@@ -12,11 +13,23 @@ public class Archer extends Unity {
 	public int getDamage() {
 		return this.damage;
 	}
+	
+	//This method asks if the archer has arrows
+	boolean haveArrows() {
+		return this.arrows > 0;
+	}
+	
+	private void reload() {
+		this.arrows = 10;
+	}
 
+	/*When the archer attacks, he/she lost one arrow. If he/she
+	doesn't have any arrow, he/she can't attack*/
 	@Override
 	public void attack(Unity u2) {
-		if (this.distanceBetween(u2) > 5.0 && this.distanceBetween(u2) < 25.0) {
+		if (haveArrows() && this.distanceBetween(u2) > 5.0 && this.distanceBetween(u2) < 25.0) {
 			u2.health -= this.damage;
+			this.arrows -= 1;
 		}
 	}
 }
--- src/eraDeImperios/Soldier.java
+++ src/eraDeImperios/Soldier.java
@@ -1,24 +1,39 @@
 package eraDeImperios;
 
-public class Soldier extends Unity{
-	
+public class Soldier extends Unity {
+
 	private int damage = 20;
-	
+	private int attacks = 5;
+
 	public Soldier(Position position) {
 		super(position);
 	}
-	
+
 	@Override
 	public int getDamage() {
 		return this.damage;
 	}
 
-	@Override
-	public void attack(Unity u2) {
-		if (this.distanceBetween(u2) < 1.5) {
-			u2.health -= this.damage;
-		}
+	// This method asks if the soldier can battle yet
+	boolean haveEnergy() {
+		return this.attacks > 0;
 	}
 
+	// This method reload the arrows of the archer
+	private void rest() {
+		this.attacks = 10;
+	}
+
+	/*
+	 * When the soldier attacks, he/she lost a little bit of energy. If he/she
+	 * doesn't have energy, he/she can't attack
+	 */
+	@Override
+	public void attack(Unity u2) {
+		if (this.haveEnergy() && this.distanceBetween(u2) < 1.5) {
+			u2.health -= this.damage;
+			this.attacks -= 1;
+		}
+	}
 
 }
